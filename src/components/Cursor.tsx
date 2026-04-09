1ef95d6f7df369380fde881c1f09c198be720c2d
Lilian <lilianperthuis@gmail.com>
2026-04-09 15:42:41 +0200

    fix(cursor): avoid cursor display on mobile device

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -7,7 +7,7 @@ export default function Cursor() {
 
   return (
     <div
-      className={`fixed mix-blend-difference opacity-75 w-12 h-12 bg-violet-600/100 blur-sm rounded-full pointer-events-none -translate-x-8 -translate-y-8 transition-transform duration-150 z-[999] ${hovering ? "scale-50" : "scale-100"}`}
+      className={`fixed cursor-custom mix-blend-difference opacity-75 w-12 h-12 bg-violet-600/100 blur-sm rounded-full pointer-events-none -translate-x-8 -translate-y-8 transition-transform duration-150 z-[999] ${hovering ? "scale-50" : "scale-100"}`}
       style={{
         left: x,
         top: y,
